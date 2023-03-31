0105b1a5024f7489743cb4c0ff018e09ea8436f1
simpsonismade24d <simpsonismade@gmail.com>
2023-03-31 15:13:18 -0700

    Readme file and task 7

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,16 +1,12 @@
 #!/usr/bin/python3
-"""displays the value of the X-Request-Id variable found in
-the header of the response.
-"""
-
+"""sends a request to the URL and displays the body of the response."""
 
 if __name__ == "__main__":
-    from requests import get
-    from sys import argv
+    import sys
+    import requests
 
-    response = get(argv[1])
+    response = requests.get(sys.argv[1])
     if response.status_code >= 400:
         print("Error code: {}".format(response.status_code))
     else:
         print(response.text)
-
--- 0x11-python-network_1/README.md
+++ 0x11-python-network_1/README.md
@@ -24,3 +24,7 @@ All your modules should have a documentation (python3 -c 'print(__import__("my_m
 You must use get to access to dictionary value by key (it won’t throw an exception if the key doesn’t exist in the dictionary)
 A documentation is not a simple word, it’s a real sentence explaining what’s the purpose of the module, class or method (the length of it will be verified)
 Your code should not be executed when imported (by using if __name__ == "__main__":)
+
+AUTHOR
+SIMPSON ISEOLUWA ILUYOMADE (simpsonismade@gmail.com)
+
